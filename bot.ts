c10f62665f4739d623d77c715db35bc9c548b27d
Hell-botz <92094675+Hell-botz@users.noreply.github.com>
2023-01-11 00:34:46 +0530

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -122,7 +122,7 @@ bot.callbackQuery("cancelLocaleSetting", async (ctx) => {
 bot.callbackQuery("helper", async (ctx) => {
   await ctx.editMessageText(
     ctx.t("help") +
-      "\n\nMovie Request Group @cine_paradise0 | Movie Updates @Cine_Paradise | Hell Botz @hell_botz).",
+      "\n\n[Movie Request Group](t.me/cine_paradise0) | [Movie Updates](t.me/Cine_Paradise) | [Hell Botz] (t.me/hell_botz).",
     {
       reply_markup: new InlineKeyboard().text("Main Menu 📭", "cancelLocaleSetting"),
       parse_mode: "HTML",
